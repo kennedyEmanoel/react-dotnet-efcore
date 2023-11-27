763134370ef2f4ea393c7be5c26fdb665732df57
Kennedy Emanoel <kennedy1emanoel19@gmail.com>
2023-11-27 19:09:21 -0300

    Add Backend

---
+++ Back/src/ProAtividade.API/Controllers/AtividadeController.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProAtividade.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AtividadeController : ControllerBase
+    {
+        [HttpGet]
+        public string get()
+        {
+            return "Get";
+        }
+
+        [HttpPost]
+        public string Post()
+        {
+            return "Post";
+        }
+
+        [HttpPut]
+        public string Put()
+        {
+            return "Put";
+        }
+
+        [HttpDelete]
+        public string Delete()
+        {
+            return "Delete";
+        }
+
+
+    }
+}
